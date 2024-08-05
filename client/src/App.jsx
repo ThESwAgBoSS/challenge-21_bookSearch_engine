7fcf76c220c609308fc425d0a1232f6d66e79d2b
James Chandler <Chandler.790j@gmail.com>
2024-08-04 21:58:37 -0400

    pages and utils finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,73 +1,35 @@
-// import user model
-const { User } = require('../models');
-// import sign token function from auth
-const { signToken } = require('../utils/auth');
+import React from 'react';
+import { ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SearchBooks from './components/SearchBooks';
+import SavedBooks from './components/SavedBooks';
+import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
+import Auth from './utils/auth';
 
-module.exports = {
-  // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
-
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
-    }
-
-    res.json(foundUser);
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: Auth.loggedIn() ? `Bearer ${Auth.getToken()}` : '',
   },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
+});
 
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route exact path="/signup" component={SignupForm} />
+            <Route exact path="/login" component={LoginForm} />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
 
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
-    console.log(user);
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
-    }
-  },
-  // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
-    }
-    return res.json(updatedUser);
-  },
-};
+export default App;
